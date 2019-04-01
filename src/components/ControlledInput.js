610210a0b1685943ded97bacb122f34af21f6d36
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-01 17:11:41 +0000

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,33 @@
 // Code ControlledInput Component Here
+import React from 'react';
+ 
+class ControlledInput extends React.Component {
+  state = {
+    value: '',
+  }
+ 
+  handleChange(event) => {
+    this.setState({value: event.target.value.toUpperCase()});
+  }
+  
+  handleSubmit(event) {
+    alert('A info was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <form onSubmit={event => this.handleSubmit(event)}>
+      <label>
+        <input type="text" value={this.state.value} onChange={this.handleChange}
+        />
+      </label>
+       <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+ 
+export default ControlledInput;
+ 
