35e69b0e2d4552bfc0fd36f1b70f98539fb2d608
Ricartho Franck <ricartho.franck@esih.edu>
2019-11-04 00:21:16 +0000

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,31 @@
 // Code ControlledInput Component Here
+import React,{Component} from 'react';
+import Form from './Form';
+
+export default class ControlledInput extends Component {
+  state = {
+    fName: "",
+    lName: ""
+  }
+
+  handleFName = (event) => {
+    this.setState({
+      fName: event.target.value
+    })
+  }
+  handleLName = (event) => {
+    this.setState({
+      lName: event.target.value
+    })
+  }
+
+  render(){
+    return(
+      <form
+        formData = {this.state}
+        handleFName = {this.handleFName}
+        handleLName = {this.handleLName}
+      />
+    )
+  }
+}
